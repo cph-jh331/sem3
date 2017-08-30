a9eb88c4b301195f7af9a657bc6fe0eb56dd3fe6
John Hansen <cph-jh331@cphbusiness.dk>
2017-08-30 20:28:45 +0200

    fetch seems confusing...

--- JSexercises/exercise4/ajax1/ajax1/src/main/webapp/myscript.js
+++ JSexercises/exercise4/ajax1/ajax1/src/main/webapp/myscript.js
@@ -1,10 +1,11 @@
 function getQuote() {
-    fetch('https://jokes-plaul.rhcloud.com/api/joke').then(function (response) {
-        response.json().then(function (data) {
-            var str = "<p1>" + data.id + " " + data.joke + "<br>ref:" + data.reference + "</p1>";
-            document.getElementById("quoteJoke").innerHTML = str;
-        });
-    });
+    fetch('https://jokes-plaul.rhcloud.com/api/joke')
+            .then(function (response) {
+                response.json().then(function (data) {
+                    var str = "<p1> Joke number " + data.id + "<br>" + data.joke + "<br>Source: " + data.reference + "</p1>";
+                    document.getElementById("quoteJoke").innerHTML = str.toString();
+                });
+            });
 }
 
 document.getElementById("myBtn").addEventListener("click", getQuote);
