bb284e6f8e162fe985b9e5b810237c37d7a9e55e
John Hansen <cph-jh331@cphbusiness.dk>
2017-08-30 20:12:21 +0200

    nemlig

--- JSexercises/exercise4/ajax1/ajax1/src/main/webapp/myscript.js
+++ JSexercises/exercise4/ajax1/ajax1/src/main/webapp/myscript.js
@@ -1,3 +1,12 @@
+function getQuote() {
+    fetch('https://jokes-plaul.rhcloud.com/api/joke').then(function (response) {
+        response.json().then(function (data) {
+            var str = "<p1>" + data.id + " " + data.joke + "<br>ref:" + data.reference + "</p1>";
+            document.getElementById("quoteJoke").innerHTML = str;
+        });
+    });
+}
 
+document.getElementById("myBtn").addEventListener("click", getQuote);
 
 
