6c055cd0793d20ec884cbd2e363b7823586f2551
John Hansen <cph-jh331@cphbusiness.dk>
2017-08-31 12:48:15 +0200

    bob

--- JSexercises/exercise4/ajax1/ajax1/src/main/webapp/myscript.js
+++ JSexercises/exercise4/ajax1/ajax1/src/main/webapp/myscript.js
@@ -1,13 +1,22 @@
 function getQuote() {
-    fetch('https://jokes-plaul.rhcloud.com/api/joke')
-            .then(function (response) {
-                response.json().then(function (data) {
-                    var str = "<p1> Joke number " + data.id + "<br>" + data.joke + "<br>Source: " + data.reference + "</p1>";
-                    document.getElementById("quoteJoke").innerHTML = str.toString();
-                });
-            });
-}
+    /*
+     fetch('https://jokes-plaul.rhcloud.com/api/joke')
+     .then(function (response) {
+     response.json().then(function (data) {
+     var str = "<p1> Joke number " + data.id + "<br>" + data.joke + "<br>Source: " + data.reference + "</p1>";
+     document.getElementById("quoteJoke").innerHTML = str.toString();
+     });
+     });*/
+    var promise = fetch('https://jokes-plaul.rhcloud.com/api/joke');
+    promise.then(function (response) {
+        return response.json();//Why return here?
+    }).then(function (quote) {
 
+        document.getElementById("joke").innerText = quote.joke;
+        document.getElementById("ref").innerText = quote.reference;
+        document.getElementById("id").innerText = quote.id;
+    });
+}
 document.getElementById("myBtn").addEventListener("click", getQuote);
 
 
